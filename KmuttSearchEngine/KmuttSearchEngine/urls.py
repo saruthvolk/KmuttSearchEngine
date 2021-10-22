0d560306a444d085f977417ffdcf1675787fb040
saruthvolk <saruth@windowslive.com>
2021-10-22 14:45:52 +0700

    CRUD OOP
    Signed-off-by: saruthvolk <saruth@windowslive.com>

--- KmuttSearchEngine/KmuttSearchEngine/urls.py
+++ KmuttSearchEngine/KmuttSearchEngine/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
-    path('insert', views.insertques, name='insertques'),
+    path('insert', views.Crud_QA, name='insertques'),
     path('about/', views.about, name='about'),
     path('login/',
          LoginView.as_view
--- KmuttSearchEngine/app/views.py
+++ KmuttSearchEngine/app/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 from KmuttSearchEngine.SearchEngine import searchengine
+from KmuttSearchEngine.Crud_QA import *
 from KmuttSearchEngine.Query import queryDb_QA
 from app.models import questionanswer
 from KmuttSearchEngine.SearchEngine import return_Result
@@ -67,26 +68,43 @@ def about(request):
 		}
 	)
 
-def insertques(request):
+def Crud_QA (request):
 
-	assert isinstance(request, HttpRequest)
-	if request.method == "POST" :
-		if request.POST.get('question_id') and request.POST.get('question') and request.POST.get('answer') and request.POST.get('question_en') and request.POST.get('answer_en') and request.POST.get('create_time') and request.POST.get('create_date') and request.POST.get('user_id') and request.POST.get('update_date') and request.POST.get('update_time') and request.POST.get('status') and request.POST.get('view_count'):
-			saverecord = questionanswer()
-			saverecord.question_id = request.POST.get('question_id')
-			saverecord.question = request.POST.get('question')
-			saverecord.answer = request.POST.get('answer')
-			saverecord.question_en = request.POST.get('question_en')
-			saverecord.answer_en = request.POST.get('answer_en')
-			saverecord.create_time = request.POST.get('create_time')
-			saverecord.create_date = request.POST.get('create_date')
-			saverecord.user_id = request.POST.get('user_id')
-			saverecord.update_date = request.POST.get('update_date')
-			saverecord.update_time = request.POST.get('update_time')
-			saverecord.status = request.POST.get('status')
-			saverecord.view_count = request.POST.get('view_count')
-			saverecord.save()
-			#messages.success(request,'question '+saverecord.question_id+ 'is saved in the datebase')#
+	operation = 'Add'
+	if operation == "Add":
+		result = Add_QA(request)
+		if result.code == 200:
 			return render(request,'app/CRUDquestion.html')
-	else:
-		return render(request,'app/CRUDquestion.html')
+		else:
+			return render(request,'app/CRUDquestion.html')
+
+	elif operation == "Edit":
+		result = Edit_QA()
+
+	elif operation == "Remove":
+		result = Remove_QA()
+
+
+#def insertques(request):
+
+#	assert isinstance(request, HttpRequest)
+#	if request.method == "POST" :
+#		if request.POST.get('question_id') and request.POST.get('question') and request.POST.get('answer') and request.POST.get('question_en') and request.POST.get('answer_en') and request.POST.get('create_time') and request.POST.get('create_date') and request.POST.get('user_id') and request.POST.get('update_date') and request.POST.get('update_time') and request.POST.get('status') and request.POST.get('view_count'):
+#			saverecord = questionanswer()
+#			saverecord.question_id = request.POST.get('question_id')
+#			saverecord.question = request.POST.get('question')
+#			saverecord.answer = request.POST.get('answer')
+#			saverecord.question_en = request.POST.get('question_en')
+#			saverecord.answer_en = request.POST.get('answer_en')
+#			saverecord.create_time = request.POST.get('create_time')
+#			saverecord.create_date = request.POST.get('create_date')
+#			saverecord.user_id = request.POST.get('user_id')
+#			saverecord.update_date = request.POST.get('update_date')
+#			saverecord.update_time = request.POST.get('update_time')
+#			saverecord.status = request.POST.get('status')
+#			saverecord.view_count = request.POST.get('view_count')
+#			saverecord.save()
+			#messages.success(request,'question '+saverecord.question_id+ 'is saved in the datebase')#
+#			return render(request,'app/CRUDquestion.html')
+#	else:
+#		return render(request,'app/CRUDquestion.html')
